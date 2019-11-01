9a402cce0dd05cebc9b04cb0d77f0d6c0669e583
Alpha Chau <chaua0927@gmail.com>
2019-10-31 21:03:37 -0400

    Added fps stats, Updated HTML

--- www/index.js
+++ www/index.js
@@ -60,14 +60,6 @@ playPauseButton.addEventListener('click', event => {
     }
 })
 
-const renderLoop = () => {
-    universe.tick();
-
-    drawGrid();
-    drawCells();
-    animationId = requestAnimationFrame(renderLoop);
-}
-
 const drawGrid = () => {
     context.beginPath();
     context.strokeStyle = GRID_COLOR;
@@ -113,6 +105,59 @@ const drawCells = () => {
     }
 
     context.stroke();
+}
+
+const fps = new class {
+    constructor() {
+        this.fpsElement = document.querySelector('#fps');
+        this.frames = [];
+        this.lastTimestamp = performance.now();
+    }
+
+    calcFPS() {
+        const now = performance.now();
+        const delta = now - this.lastTimestamp;
+        this.lastTimestamp = now;
+        return 1 / delta * 1000
+    }
+
+    render() {
+        const fps = this.calcFPS();
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift;
+        }
+
+        // Calculate stats: min, max, mean
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Display stats
+        this.fpsElement.textContent = `
+        Frames per second:
+                latest = ${Math.round(fps)}
+        avg (last 100) = ${Math.round(mean)} 
+        max (last 100) = ${Math.round(max)}
+        min (last 100) = ${Math.round(min)}
+        `;
+    }
+}
+
+const renderLoop = () => {
+    fps.render();
+
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+    animationId = requestAnimationFrame(renderLoop);
 }
 
 play();
